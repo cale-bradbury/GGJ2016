da5d504a951bef57b38739f21e0cee6269cce835
Erik Krieg <erik.c.krieg@gmail.com>
2016-01-30 04:59:43 -0500

    Elevator now has movement. Levels are toggled halfway through this state.

--- Assets/Scripts/ElevatorController.cs
+++ Assets/Scripts/ElevatorController.cs
@@ -7,11 +7,19 @@ public class ElevatorController : MonoBehaviour {
     public List<GameObject> levelPrefabs = new List<GameObject>();
     private List<GameObject> levels = new List<GameObject>();
     private GameObject activeLevel;
+    private GameObject nextLevel;
     private Vector3 levelOrigin = new Vector3(0f, 0f, 0f);
+
+    bool isMoving = false;
+    float maxElevatorRideDuration = 10f;
+    float halfElevatorRideDuration;
+    float timeLeftInMotion = 0f;
 
     // Use this for initialization
     void Start () {
-        foreach(GameObject levelPrefab in levelPrefabs)
+        halfElevatorRideDuration = maxElevatorRideDuration / 2f;
+
+        foreach (GameObject levelPrefab in levelPrefabs)
         {
             GameObject level = (GameObject)Instantiate(levelPrefab, levelOrigin, Quaternion.identity);
             level.SetActive(false);
@@ -19,22 +27,58 @@ public class ElevatorController : MonoBehaviour {
         }
     }
 
-    // Update is called once per frame
-    void Update() {
 
+    void Update() {
+        MoveElevator();
     }
 
-    public void GoToLevel (int levelKey) {
-        Debug.Log("Go to: " + levelKey);
-        GameObject requestedLevel = levels[levelKey - 1];
-        if (activeLevel) {
-            if(activeLevel != requestedLevel) {
-                activeLevel.SetActive(false);
-            } else {
-                return;
+    public void GoToLevel(int levelKey) {
+        if (!isMoving)
+        {
+            Debug.Log("Go to: " + levelKey);
+            GameObject requestedLevel = levels[levelKey - 1];
+            bool isNotSameFloor = activeLevel && activeLevel != requestedLevel;
+            if (!activeLevel || isNotSameFloor)
+            {
+                nextLevel = requestedLevel;
+                StartMoving();
+            }
+        }        
+    }
+
+    void MoveElevator()
+    {
+        if (isMoving)
+        {
+            timeLeftInMotion -= Time.deltaTime;
+
+            if (timeLeftInMotion <= halfElevatorRideDuration && nextLevel)
+            {
+                if (activeLevel){
+                    activeLevel.SetActive(false);                    
+                }
+                activeLevel = nextLevel;
+                nextLevel = null;
+                activeLevel.SetActive(true);
+                Debug.Log("Half way there");            
+            }
+
+            if (timeLeftInMotion <= 0f)
+            {
+                Debug.Log("Arrived");
+                StopMoving();
             }
         }
-        activeLevel = requestedLevel;
-        activeLevel.SetActive(true);
+    }
+
+    void StartMoving() {
+        timeLeftInMotion = maxElevatorRideDuration;
+        isMoving = true;
+    }
+
+    void StopMoving() {
+        isMoving = false;
+        timeLeftInMotion = 0f;        
     }
 }
+
