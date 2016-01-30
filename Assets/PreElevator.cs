48b4a36894334fe877123fdf237a28e7c1fe7a87
netgrind <cale.bradbury@gmail.com>
2016-01-30 14:48:14 -0500

    leveldata

---
+++ Assets/PreElevator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+[ExecuteInEditMode]
+public class PreElevator : ImageEffectBase {
+
+	RenderTexture pure;
+
+	// Called by camera to apply image effect
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+		Graphics.Blit (source, destination,material);
+		//Graphics.Blit (source, pure);
+	}
+}
---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LevelData : MonoBehaviour {
+
+	public string levelName = "default level";
+	public Material skybox;
+	public Color ambientSky;
+	public Color ambientEquator;
+	public Color ambientGround;
+	public Color fogColor;
+	public float fogStartDistance;
+	public float fogEndDistance;
+	public bool constantUpdate = false;
+
+	// Use this for initialization
+	void OnEnable () {
+		RenderSettings.skybox = skybox;
+		RenderSettings.fogColor = fogColor;
+		RenderSettings.fogStartDistance = fogStartDistance;
+		RenderSettings.fogEndDistance = fogEndDistance;
+		RenderSettings.fogMode = FogMode.Linear;
+		RenderSettings.fog = true;
+		RenderSettings.ambientSkyColor = ambientSky;
+		RenderSettings.ambientEquatorColor = ambientEquator;
+		RenderSettings.ambientGroundColor = ambientGround;
+		RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Trilight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(constantUpdate)OnEnable ();
+	}
+
+	void OnGUI(){
+		OnEnable ();
+	}
+}
