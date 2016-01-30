0aebc252430bb834fd0892e7568866800da70940
netgrind <cale.bradbury@gmail.com>
2016-01-30 17:20:36 -0500

    custom post effects on a per scene basis

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.ImageEffects;
 
 [ExecuteInEditMode]
 public class LevelData : MonoBehaviour {
@@ -13,9 +14,39 @@ public class LevelData : MonoBehaviour {
 	public float fogStartDistance;
 	public float fogEndDistance;
 	public bool constantUpdate = false;
+	public Camera postCam;
+	ImageEffectBase[] fx;
 
 	// Use this for initialization
 	void OnEnable () {
+		UpdateRenderSettings ();
+		UpdateCamera ();
+	}
+
+	void OnDisable(){
+		RemoveCamera ();
+	}
+
+	void UpdateCamera(){
+		if (!Application.isPlaying)
+			return;
+		if (postCam == null)
+			return;
+		fx = postCam.GetComponents<ImageEffectBase> ();
+		foreach (ImageEffectBase i in fx)
+			Utils.MoveComponent (i, Camera.main.gameObject);
+		Camera.main.gameObject.AddComponent<PostElevator> ();
+		postCam.enabled = false;
+	}
+
+	void RemoveCamera(){
+		fx = Camera.main.GetComponents<ImageEffectBase> ();
+		foreach (ImageEffectBase i in fx) {
+			Destroy (i);
+		}
+	}
+
+	void UpdateRenderSettings(){
 		RenderSettings.skybox = skybox;
 		RenderSettings.fogColor = fogColor;
 		RenderSettings.fogStartDistance = fogStartDistance;
@@ -30,10 +61,10 @@ public class LevelData : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(constantUpdate)OnEnable ();
+		if(constantUpdate)UpdateRenderSettings ();
 	}
 
 	void OnGUI(){
-		OnEnable ();
+		UpdateRenderSettings ();
 	}
 }
