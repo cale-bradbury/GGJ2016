77fd49bc1c69727bff194d8b6a6745fe2e3c0242
netgrind <cale.bradbury@gmail.com>
2016-01-30 14:54:50 -0500

    post elevator

--- Assets/PostElevator.cs
+++ Assets/PostElevator.cs
@@ -8,7 +8,7 @@ public class PostElevator : ImageEffectBase {
 	PreElevator pre;
 
 	void OnEnable(){
-		pre = GetComponent (PreElevator);
+		pre = GetComponent<PreElevator>();
 	}
 
 	// Called by camera to apply image effect
