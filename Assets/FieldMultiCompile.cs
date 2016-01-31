9b30ccc978be956758f870271d7df5be82f300a6
netgrind <cale.bradbury@gmail.com>
2016-01-30 21:58:42 -0500

    materials and bron time saver

---
+++ Assets/FieldMultiCompile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class FieldMultiCompile : MonoBehaviour {
+
+	public enum Style{
+		sphere,
+		cross2,
+		cross1 ,
+		cube,
+		hex,
+		tie,
+		tri
+	}
+
+	public Style style = Style.sphere;
+	public Material mat;
+
+	// Use this for initialization
+	void Set () {
+		foreach(string s in mat.shaderKeywords)
+			mat.DisableKeyword (s);
+		mat.EnableKeyword (style.ToString ().ToLower());
+		Debug.Log (style.ToString ().ToLower());
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		Set ();
+	}
+
+	void OnEnable(){
+		Set ();
+	}
+}
