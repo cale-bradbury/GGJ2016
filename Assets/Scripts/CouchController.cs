95d310b67dc8c0f5ec0ffa6cd4a68cdde4d1c198
Erik Krieg <erik.c.krieg@gmail.com>
2016-01-31 08:23:53 -0500

    added couch controller. More dialog for Pal.

---
+++ Assets/Scripts/CouchController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CouchController : MonoBehaviour {
+
+    private bool isPlayerOnCouch = false;
+    public bool isPalOnCouch = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("player enter couch");
+            isPlayerOnCouch = true;
+        }
+
+        if(isPlayerOnCouch && isPalOnCouch)
+        {
+            // trigger anim float for RGB rotate
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("player exit couch");
+            isPlayerOnCouch = false;
+        }
+    }
+}
