0bfd993a8f8e37e4d5619bca3d7fa360e8559970
netgrind <cale.bradbury@gmail.com>
2016-01-31 15:36:36 -0500

    new vator

---
+++ Assets/RandomizeAudio.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeAudio : MonoBehaviour {
+
+	AudioSource source;
+	public AudioClip[] elevatorClips;
+
+	// Use this for initialization
+	void OnEnable() {
+		source = GetComponent<AudioSource>();
+		Randomize();
+	}
+
+	void OnDisable(){
+		CancelInvoke("Randomize");
+	}
+	
+	// Update is called once per frame
+	void Randomize () {
+		CancelInvoke("Randomize");
+		source.clip = elevatorClips[Mathf.FloorToInt(Random.value*elevatorClips.Length)];
+		source.Play();
+		Invoke("Randomize",source.clip.length);
+	}
+}
