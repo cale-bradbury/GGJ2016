bfe7b1fdec188b9a4dbc14248471cf6629ed0a4c
Erik Krieg <erik.c.krieg@gmail.com>
2016-01-31 15:10:30 -0500

    Added trigger warnings for PAL.

--- Assets/Scripts/Pal/PalController.cs
+++ Assets/Scripts/Pal/PalController.cs
@@ -9,21 +9,27 @@ public class PalController : MonoBehaviour {
     public ParticleSystem particles;
     public CouchController couchController;
     public AudioSource[] sounds;
+    public AudioSource teleportSound;
     public float speed = 0.1f;
     public float minDistance = 2f;
     private bool canFollow = false;
+    private bool isCloseWithDave = false;
+    private bool isWorried = false;
 
     public Dialog introTalk;
     public Dialog couchTalk;
     public Dialog leavingTalk;
+    public Dialog worriedTalk;
 
     void Start()
     {
         couchTalk.enabled = false;
         leavingTalk.enabled = false;
+        worriedTalk.enabled = false;
         particles.playOnAwake = true;
         Messenger.AddListener("pal-to-couch", TeleportToCouch);
         Messenger.AddListener("pal-enable-couch-talk", EnableCouchTalk);
+        Messenger.AddListener("isCloseWithDave", CloseWithDave);
     }
 
     // Update is called once per frame
@@ -33,7 +39,22 @@ public class PalController : MonoBehaviour {
         {
             transform.LookAt(pos);
         }
+        Worrying();
         Follow();
+    }
+
+    void Worrying() {
+        if (couchController.isPalOnCouch && isCloseWithDave)
+        {
+            float distance = getDistance();
+
+            if(distance > 10f)
+            {
+                Debug.Log("PAL is starting to get worried.");
+                ResumeFollowing();
+                EnableWorriedTalk();
+            }
+        }
     }
 
     void Follow() {
@@ -76,6 +97,7 @@ public class PalController : MonoBehaviour {
     }
 
     void TeleportParticles() {
+        teleportSound.Play();
         particles.gameObject.SetActive(false);
         particles.gameObject.SetActive(true);
     }
@@ -87,6 +109,30 @@ public class PalController : MonoBehaviour {
     void EnableCouchTalk() {
         introTalk.enabled = false;
         leavingTalk.enabled = false;
+        worriedTalk.enabled = false;
         couchTalk.enabled = true;
+    }
+
+    void EnableWorriedTalk() {
+        introTalk.enabled = false;
+        leavingTalk.enabled = false;
+        worriedTalk.enabled = true;
+        couchTalk.enabled = false;
+        isWorried = true;
+    }
+
+    void CloseWithDave() {
+        isCloseWithDave = true;
+        //couchTalk.enabled = false;    // causes the dialog not to wrap up interaction correctly (text box remains visible)
+    }
+
+    public void SetThreatLevel(int threatLevel)
+    {
+
+    }
+
+    public bool getIsWorried()
+    {
+        return isWorried;
     }
 }
